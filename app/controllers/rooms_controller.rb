573a3226e3fc57261153a8951284d723d13d164d
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2021-10-18 15:17:23 +0300

    ✨ Enable room creation from UI

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,13 @@
 class RoomsController < ApplicationController
+  def create
+    @room = Room.create(name: params['room']['name'])
+  end
+
   def index
     @current_user = current_user
     redirect_to signin_path unless @current_user
     @rooms = Room.public_rooms
     @users = User.all_except(@current_user)
+    @room = Room.new
   end
 end
