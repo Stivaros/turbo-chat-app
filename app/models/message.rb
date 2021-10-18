34d2238d472a779d0ea5d25c75c2b06fdd1aa527
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2021-10-18 17:17:43 +0300

    ✨ Enable private messaging

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,15 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
 
+  before_create :confirm_participant
   after_create_commit { broadcast_append_to self.room }
+
+  private
+
+  def confirm_participant
+    if self.room.private
+      participant = Participant.where(user_id: self.user.id, room_id: self.room.id).first
+      throw :abort unless participant
+    end
+  end
 end
